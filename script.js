87cefd58ff062a80e0b3bc91f813bc805a48934f
damarissulser <damaris.sulser@stud.fhgr.ch>
2023-12-05 10:10:27 +0100

    Update

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ document.addEventListener('mousemove', function(e) {
         hsl(${percentageX}, 100%, 50%), 
         hsl(${percentageY}, 100%, 50%))`;
 })
+
+
+
+function toggleExpandedText() {
+    var expandedText = document.getElementById('expandedText');
+    var moreInfoButton = document.getElementById('moreInfoButton');
+
+    // Überprüfe, ob der erweiterte Text sichtbar ist
+    if (window.getComputedStyle(expandedText).display === 'none') {
+        // Wenn unsichtbar, mache ihn sichtbar
+        expandedText.style.display = 'block';
+        // Ändere den Text im Button
+        moreInfoButton.innerHTML = 'Weniger anzeigen';
+    } else {
+        // Wenn sichtbar, mache ihn unsichtbar
+        expandedText.style.display = 'none';
+        // Ändere den Text im Button
+        moreInfoButton.innerHTML = 'Mehr Infos';
+    }
+}
+
