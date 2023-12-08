fc0f745f0393f05bff8254be960001aea00ff498
damarissulser <damaris.sulser@stud.fhgr.ch>
2023-12-08 06:52:54 +0100

    Updates

--- script.js
+++ script.js
@@ -5,12 +5,10 @@ document.addEventListener('mousemove', function(e) {
     // Berechne den Prozentsatz der Mausposition im Verhältnis zur Bildschirmhöhe
     const percentageY = (e.clientY / window.innerHeight) * 100;
 
-    // Setze die Hintergrundfarbe mit einem linearen Gradienten basierend auf der Mausposition
     document.body.style.background = `linear-gradient(to right, 
-        hsl(${percentageX}, 100%, 50%), 
-        hsl(${percentageY}, 100%, 50%))`;
+        hsl(${240 + percentageX}, 100%, 50%), 
+        hsl(${300 + percentageY}, 100%, 50%))`;
 })
-
 
 
 function toggleExpandedText() {
@@ -30,4 +28,3 @@ function toggleExpandedText() {
         moreInfoButton.innerHTML = 'Mehr Infos';
     }
 }
-
